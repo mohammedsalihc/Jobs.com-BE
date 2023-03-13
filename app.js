64d4e2ef9941b0673e04b6ab0c9578c4749c5987
Mohammedsalihmac@gmail.com <mohammedsalihmac@gmail.com>
2023-03-13 16:27:24 +0530

    changes

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const app = express()
 const cors = require('cors')
 const userRouter = require('./routes/userRouter')
 const port = process.env.PORT || 4000
+const FRONT_URL = "https://fabulous-manatee-15ce10.netlify.app"
 const cookieParser = require("cookie-parser")
 require('dotenv').config();
 require('./db/connections')
-app.use(cors({ credentials: true, origin: `${process.env.FRONTENT_URL}` }))
+app.use(cors({ credentials: true, origin: `${FRONT_URL}` }))
 app.use(express.json())
 app.use(cookieParser())
 app.use("/", userRouter)
